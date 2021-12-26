f8bd11eb29d02a33dd6a70de9f5db2b75b623544
arslnn <arsalanrashidthoker@gmail.com>
2021-12-27 01:19:17 +0530

    added tests for truth
    table

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,7 @@
 #include <vector>
 #include <deque>
 #include <queue>
+#include <chrono>
 #include "./headers/Bool.h"
 #include "./headers/get_res.h"
 #include "./headers/display_row.h"
@@ -25,13 +26,21 @@ void print_table(string inp){
     vector <Bool> B;
     for(int i=0;i<(1 << var_size);i++){
         B = evaluate(inp, i, var);
-         //display_row(B);
+         display_row(B);
     }
-    display_row(B);
+  return;
 }
 int main(){
    string s;
    cin>>s;
+   auto start = std::chrono::steady_clock::now();
    print_table(s);
+   auto end = std::chrono::steady_clock::now();
+   auto elapsed_ms = std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
+   auto elapsed_s = std::chrono::duration_cast<std::chrono::seconds>(end - start);
+   cout<<"\nTime taken in ms: \n";
+   cout<<elapsed_ms.count()<<endl;
+   cout<<"\nTime taken in ss: \n";
+   cout<<elapsed_s.count()<<endl;
    return 0;
 }
